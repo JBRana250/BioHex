40adf7e298b7f4f335b984c8588e196bd621a3e0
JB Rana <JBRana2500@gmail.com>
2024-08-03 17:02:08 +1200

    fixed transparency issues

--- World Scene/scripts/room scripts/OnClickComponentScript.gd
+++ World Scene/scripts/room scripts/OnClickComponentScript.gd
@@ -5,10 +5,11 @@ extends Node
 
 func _on_area_3d_input_event(_camera, event, _position, _normal, _shape_idx):
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			print(attributes.row_num)
-			print(attributes.room_pos)
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true and attributes.eligible_to_travel:
+		#if Input.is_action_just_pressed("left_click"):
+			#print_debug(attributes.row_num)
+			#print_debug(attributes.room_pos)
+			#print_debug(attributes.eligible_to_travel)
+		if Input.is_action_just_pressed("left_click") and attributes.eligible_to_travel:
 			Globals.current_room_pos = attributes.room_pos
 			Globals.current_row = attributes.row_num
 			SceneSwitcher.switch_scene(attributes.res_path)
--- World Scene/scripts/room scripts/RoomAppearance.gd
+++ World Scene/scripts/room scripts/RoomAppearance.gd
@@ -1,11 +1,12 @@
 extends Node
 
 @onready var attributes = get_parent().get_node("RoomAttributes")
-@onready var room_mesh = owner.get_node("room_mesh").get_child(0)
+@onready var room_mesh = owner.get_node("room_mesh")
 
 func _ready():
 	if attributes.eligible_to_travel:
-		for surface_override_material_index in range(room_mesh.get_surface_override_material_count()):
-			var surface_override_material = room_mesh.get_surface_override_material(surface_override_material_index)
-			var material_color = surface_override_material.albedo_color
-			room_mesh.get_surface_override_material(surface_override_material_index).albedo_color = Color(material_color.r, material_color.g, material_color.b, 255)
+		for room_mesh_child in room_mesh.get_children():
+			for surface_override_material_index in range(room_mesh_child.get_surface_override_material_count()):
+				var surface_override_material = room_mesh_child.get_surface_override_material(surface_override_material_index)
+				var material_color = surface_override_material.albedo_color
+				room_mesh_child.get_surface_override_material(surface_override_material_index).albedo_color = Color(material_color.r, material_color.g, material_color.b, 255)
